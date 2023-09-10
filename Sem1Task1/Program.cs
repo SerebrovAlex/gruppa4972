1450440a139ec80ec9694d012c3daa2e06a0a6fa
alex serebrov <alex.u.serebrov@gmail.com>
2023-09-10 14:25:44 +0300

    solution from sem1 task 0,1,3,5

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число А, ");
+int numA = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число B, ");
+int numB = int.Parse(Console.ReadLine() ?? "0");
+if (numB * numB == numA)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("НЕТ");
+}
---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число, ");
+int numN = int.Parse(Console.ReadLine() ?? "0");
+int evNumN = (-1) * numN;
+while (evNumN < numN)
+{
+    Console.WriteLine(evNumN + ",");
+    evNumN = evNumN + 1;
+}
+Console.WriteLine(numN);
