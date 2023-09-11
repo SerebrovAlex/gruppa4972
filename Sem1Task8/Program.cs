e3300a26ceb1e37cf369524ac120c1e6265e3cd8
alex serebrov <alex.u.serebrov@gmail.com>
2023-09-11 12:49:12 +0300

    ispravlenia

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введите число, ");
+﻿Console.WriteLine("Введите число");
 int num = int.Parse(Console.ReadLine() ?? "0");
 int count = 0;
 if (num % 2 == 0)
@@ -6,7 +6,7 @@ if (num % 2 == 0)
     while (count < num)
     {
         count = count + 2;
-        Console.WriteLine(count);
+        Console.Write((count) + ", ");
     }
 }
 else
@@ -16,6 +16,25 @@ if (num % 2 == 1)
     while (count < num)
     {
         count = count + 2;
-        Console.WriteLine(count);
+        Console.Write((count) + ", ");
     }
 }
+
+// Console.Write("Введите число: "); // Создаем строку ввода
+// string? inputlineN = Console.ReadLine(); // Инициализируем сканер
+// int startNumber = 2; // Переменна, с кот начинается отчет
+
+// if (inputlineN != null) // Проверяем, что строка не пустая
+// {
+// int inputnumberN = int.Parse(inputlineN); // Парсим строку в инт
+// while (startNumber <= inputnumberN - 2) // Условие завершения цикла
+// {
+// Console.Write(startNumber + ", "); // Если число делится - выводим в консоль
+// startNumber = startNumber + 2; // прибавляем единицу к числу
+// }
+
+// if (startNumber % 2 == 0)
+// {
+//     Console.WriteLine(startNumber);
+// }
+// }
